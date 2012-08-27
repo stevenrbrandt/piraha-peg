da7d0043755aaeb48b5e3ec1362c3452f8ce2a89
steven.brandt@gmail.com <steven.brandt@gmail.com@1a70d7c6-ffe7-ef14-4583-3269c1a2e543>
2012-08-27 20:22:02 +0000

    Remove unused import
    git-svn-id: https://piraha-peg.googlecode.com/svn/trunk@60 1a70d7c6-ffe7-ef14-4583-3269c1a2e543

--- src/edu/lsu/cct/piraha/examples/Generic.java
+++ src/edu/lsu/cct/piraha/examples/Generic.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.StringWriter;
 
 import edu.lsu.cct.piraha.DebugOutput;
 import edu.lsu.cct.piraha.Grammar;
