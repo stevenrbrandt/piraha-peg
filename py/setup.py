c31f15d9d6df8d6976d8ee9779be6837251aa30a
Steven R. Brandt <sbrandt@cct.lsu.edu>
2022-10-05 14:57:39 -0500

    Add setup.py

---
+++ py/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='Piraha',
+  version='1.0.0',
+  description='Parsing Expression Grammar for Python',
+  long_description='Parsing Expression Grammar for Python',
+  url='https://github.com/stevenrbrandt/piraha-peg.git',
+  author='Steven R. Brandt',
+  author_email='steven@stevenrbrandt.com',
+  license='LGPL',
+  packages=['piraha']
+)
